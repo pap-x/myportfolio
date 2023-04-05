59f6457f91532a88ee4a1c2e23d89d8fc86b5224
Chris Papadopoulos <chrspapadop@gmail.com>
2023-04-05 14:51:12 +0300

    favicon fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="./favicon.ico" />
           <title>CP Portfolio</title>
         </Head>
         <body>
