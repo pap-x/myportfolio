9f61d9f8707fe1eae79b26962b91d3c6961a38a2
Chris Papadopoulos <chrspapadop@gmail.com>
2023-04-05 14:08:37 +0300

    learn more button fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => (
       Hello, I am Chris Papadopoulos and I am a full-stack developer with a strong background in Computer Engineering and a passion for building 
       intuitive and scalable web-based solutions. Proficient in a wide range of web technologies, languages and frameworks, I am comfortable working 
       on both the front-end and back-end of web applications. I have a proven track record of successfully delivering projects from concept to deployment.      </SectionText>
-      <Button onClick={() => window.location = 'https://google.com'} alt>Learn More</Button>
+      <Button onClick={() => window.open('https://www.linkedin.com/in/chrispapadop/', '_blank')} alt>Learn More</Button>
     </LeftSection>
   </Section>
 );
